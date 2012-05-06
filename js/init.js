9280ff0f30bb72d4289dde5cd4a92168a1a04aa7
Alberto Blázquez <hola@albertoblazquez.net>
2012-05-06 23:30:08 +0200

    2 new UP constants: initial slider value and the method's height (to perform the fix of the method-info animation)

--- js/init.js
+++ js/init.js
@@ -2,13 +2,12 @@
 	var UP = UP || {};	
 	window.UP = UP;
 
-
 	/*
 	 * What the framework calls 'Views' are actually 'Controllers' 
 	 * so we rename the module by creating an object Controller that inherits every property of View
 	 */
 	Backbone.Controller = Backbone.View.extend({});
- 
+
 
 	/* Constants */
 	UP.constants = {
@@ -16,7 +15,9 @@
 		DISABLED_OPACITY : 0.5,
 		FADE_SPEED       : 700,
 		MAX_WEIGHT_VALUE : 5,
+		METHOD_HEIGHT    : 25,
 		SLIDE_SPEED      : 300,
+		SLIDER_VALUE     : 1,
 		STAGES_SPEED     : 500,
 		VALUE            : [ "strongly recommended", "neutral", "slightly recommended", "not recommended" ]
 	};
